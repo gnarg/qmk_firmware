5f3d16477bdc0136d6e8c2941109ad021c6aa6c6
MaaS Team <maas-team@newrelic.com>
2020-04-25 12:28:44 -0700

    add del key

--- keyboards/gergo/keymaps/gnarg/keymap.c
+++ keyboards/gergo/keymaps/gnarg/keymap.c
@@ -47,7 +47,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                    | LGUI     |   | Lower |                                | Raise |   | Alt |
  *                    '----------'   '-------'                                `-------.   '-----'
  *                                        ,-------.                      ,-------.
- *                                        |  (    |                      |  )    |
+ *                                        |  (    |                      |  Del  |
  *                                 ,------|-------|                      |-------|------.
  *                                 |      |       |                      |       |      |
  *                                 | Ctrl/|  Tab  |                      | BckSp |Space |
@@ -57,7 +57,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_BASE] = LAYOUT_gergo(
     KC_ESC,   KC_Q,  KC_W,   KC_E,  KC_R, KC_T,                                          KC_Y,  KC_U, KC_I,    KC_O,   KC_P,    KC_BSLS,
     KC_LSFT,  KC_A,  KC_S,   KC_D,  KC_F, KC_G, KC_LBRC,                       KC_RBRC,  KC_H,  KC_J, KC_K,    KC_L,   KC_SCLN, KC_QUOT,
-    KC_LCTRL, KC_Z,  KC_X,   KC_C,  KC_V, KC_B, KC_LCBR, KC_LPRN,     KC_RPRN, KC_RCBR,  KC_N,  KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
+    KC_LCTRL, KC_Z,  KC_X,   KC_C,  KC_V, KC_B, KC_LCBR, KC_LPRN,     KC_DEL,  KC_RCBR,  KC_N,  KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
                      KC_LGUI, LOWER, MT(MOD_RCTL, KC_ENT), KC_TAB,    KC_BSPC, KC_SPC,   RAISE, KC_RALT
     ),
 /* Keymap 2: Lower layer
